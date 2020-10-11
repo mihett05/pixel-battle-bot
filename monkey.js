bd2aa83231e66f101c985402fb92faad5a48e26b
mihett05 <mihett05@yandex.ru>
2020-10-11 12:25:38 +0500

    Added converter

---
+++ monkey.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name     PixelBattle2020
+// @version  0.1
+// @author   mihett05
+// @include  https://vk.com/*
+// ==/UserScript==
+
+
---
+++ utils/convert.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const { createCanvas, loadImage } = require('canvas');
+const { decode_colors, colors } = require('./colors');
+
+
+const startCoord = {
+	x: process.argv[3] || 154,
+	y: process.argv[4] || 137
+}
+
+const convert = async () => {
+  const realImg = await loadImage(process.argv[2] || 'image.png');
+
+  const canvas = createCanvas(realImg.width, realImg.height);
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(realImg, 0, 0, realImg.width, realImg.height);
+
+  const img = ctx.getImageData(0, 0, realImg.width, realImg.height).data;
+  const dataToDraw = [];
+
+  
+  for (let y = 0; y < realImg.height; y++) {
+    for (let x = 0; x < realImg.width * 4; x += 4 * 10) {
+      const offset = x + (y * realImg.width * 4 * 10);
+      const color = [img[offset], img[offset + 1], img[offset + 2]];
+
+      if (img[offset + 3] < 1) continue;  // Если альфа-прозрачность = 0
+      else {
+        for (const colord of colors) {
+          if (color[0] === colord[0] && color[1] === colord[1] && color[2] === colord[2]) {
+            if (!(y in dataToDraw)) dataToDraw[y] = [];
+            dataToDraw[y][x / 40] = colord[3];
+            break;
+          }
+        }
+      }
+    }
+  }
+  const repr = dataToDraw.map(v => v.join(',') + ',').join('\n');
+  fs.writeFileSync('../image.js', `var _rustbot_image_data = [${repr}]`);
+}
+convert();
+
