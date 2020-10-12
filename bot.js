3fc867a9c145feb5b344b926ae02ce315ef122fb
mihett05 <mihett05@yandex.ru>
2020-10-12 12:22:29 +0500

    Finished bot

--- bot.js
+++ bot.js
@@ -39,19 +39,23 @@ const decode_colors = {
   p: 25,
 };
 
+const MAX_WIDTH = 1590;
+const MAX_HEIGHT = 400;
 
 const Data = {
-  MAX_WIDTH: 1590,
-  MAX_HEIGHT: 400,
   MAX_COLOR_ID: 25,
   MIN_COLOR_ID: 0,
 
-  SIZE = Data.MAX_WIDTH * Data.MAX_HEIGHT,
+  SIZE: MAX_WIDTH * MAX_HEIGHT,
   SEND_PIXEL: 0,
+
   ws: null,
+  startWait: 0,
+  startWaitGot: false,
 
   map: {},
   image: {},
+  start: {},
 
   randomInteger: function (min, max) {
     const rand = min - 0.5 + Math.random() * (max - min + 1);
@@ -59,7 +63,7 @@ const Data = {
   },
   randomPixel: function () {
     
-  }
+  },
   chunkString: function (str, length) {
     return str.match(new RegExp('.{1,' + length + '}', 'g'));
   },
@@ -69,15 +73,15 @@ const Data = {
   },
   pack: function (colorId, flag, x, y) {
     const b = parseInt(colorId, 10) + parseInt(flag, 10) * Data.MAX_COLOR_ID;
-    return parseInt(x, 10) + parseInt(y, 10) * Data.MAX_WIDTH + Data.SIZE * b;
+    return parseInt(x, 10) + parseInt(y, 10) * MAX_WIDTH + Data.SIZE * b;
   },
 
   unpack: function (b) {
     const c = Math.floor(b / Data.SIZE);
-    const d = (b -= c * Data.SIZE) % Data.MAX_WIDTH;
+    const d = (b -= c * Data.SIZE) % MAX_WIDTH;
     return {
       x: d,
-      y: (b - d) / Data.MAX_WIDTH,
+      y: (b - d) / MAX_WIDTH,
       color: c % Data.MAX_COLOR_ID,
       flag: Math.floor(c / Data.MAX_COLOR_ID),
     };
@@ -114,12 +118,35 @@ const Data = {
 
     return array;
   },
-  send: function (ws, colorId, flag, x, y, store) {
-    const c = new ArrayBuffer(4);
-    new Int32Array(c, 0, 1)[0] = Data.pack(colorId, flag, x, y);
+  parseEvent: function (event) {
+    if (typeof event === 'string') return;
 
-    Data.ws.send(c);
-    console.log(`Был раскрашен пиксель [${x}, ${y}] (${colorId})`);
+    const c = Data.toArrayBuffer(event.data);
+
+    for (let d = c.byteLength / 4, e = new Int32Array(c, 0, d), f = Math.floor(d / 3), g = 0; g < f; g++) {
+      const h = e[3 * g],
+        k = Data.unpack(h),
+        l = k.x,
+        m = k.y,
+        n = k.color;
+      console.log(k);
+      Data.map[[l, m]] = n;
+    }
+  },
+  send: function (colorId, flag, x, y) {
+    if (Data.map[[x, y]] !== colorId) {
+      const c = new ArrayBuffer(4);
+      new Int32Array(c, 0, 1)[0] = Data.pack(colorId, flag, x, y);
+
+      if (Data.ws !== null) {
+        Data.ws.send(c);
+        Data.map[[x, y]] = colorId;
+        console.log(`Был раскрашен пиксель [${x}, ${y}] (${colorId})`);
+        return true;
+      }
+      
+    }
+    return false;
   }
 };
 
@@ -147,36 +174,117 @@ async function loadMap() {
 }
 
 
+async function loadImage() {
+  const req = await fetch('https://raw.githubusercontent.com/mihett05/pixel-battle-bot/master/image.json');
+  const data = await req.json();
+  Data.start = data.start;
+  Data.image = data.img;
+}
 
 
-function onOpen() {
-  console.log('opened', mapData);
+function pixelHandler() {
+  for (let y = 0; y < Data.image.length; y++) {
+    for (let x = 0; x < Data.image[y].length; x++) {
+      const color = Data.image[y][x];
+      const mapCoord = {
+        x: x + Data.start.x,
+        y: y + Data.start.y
+      };
+      
+      if (Data.map[[mapCoord.x, mapCoord.y]] !== color) {
+        console.log('found', mapCoord, Data.map[[mapCoord.x, mapCoord.y]], color);
+        Data.send(color, Data.SEND_PIXEL, mapCoord.x, mapCoord.y);
+        return;
+      }
+    }
+  }
+}
+
+
+async function onOpen() {
+  if (Data.startWaitGot) {
+    console.log('opened wait', Data.startWait);
+    await Data.sleep(Data.startWait);
+    console.log('waited start');
+    while (true) {
+      console.log('iter');
+      pixelHandler();
+      console.log('sleep');
+      await Data.sleep(60200);
+    }
+  } else {
+    setTimeout(() => {
+      onOpen();
+    }, 60200);
+  }
+  
+}
+
+function earlyOnMessage(msg) {
+  if (typeof msg.data === 'string') {
+    try {
+      const data = JSON.parse(msg.data);
+      if (data.t === 12 && data.v[0] && data.v[0].v.wait !== undefined) {
+        Data.startWait = data.v[0].v.wait;
+        Data.startWaitGot = true;
+      }
+      console.log(data);
+    } catch (e) {}
+  }
 }
 
 function onMessage(msg) {
-  
+  if (typeof msg.data === 'string') {
+    try {
+      const data = JSON.parse(msg.data);
+      console.log(data);
+    } catch (e) {
+      console.log(msg.data);
+    }
+  } else {
+    Data.parseEvent(msg);
+  }
 }
 
-function onError() {
-  
+function onError(err) {
+  console.log('err', err);
 }
 
-function onClose() {
-  
+function onClose(event) {
+  console.log('close', event);
+  Data.ws = null;
+  window.location.reload();
 }
-
 
 const oldWs = unsafeWindow.WebSocket;
 
 unsafeWindow.WebSocket = function newWs(url, protocol) {
   console.log(url, url.includes('pixel'));
   if (url.includes('pixel')) {
+    if (Data.ws !== null) {
+      if (Data.ws.readyState !== 3) {
+        Data.ws.close();
+      }
+      Data.ws = null;
+    }
     const ws = new oldWs(url, protocol);
+    
+    Data.ws = ws;
     console.log(ws);
-    Promise.all([loadData(), ])
+
+    
+    let oldOnMessage = null;
+    const messageCallbacks = [];
+    
+
+    setTimeout(() => {
+      oldOnMessage = ws.onmessage;
+      ws.onmessage = combine(earlyOnMessage, oldOnMessage);
+    }, 40);
+
+    Promise.all([loadMap(), loadImage()])
       .then(() => {
         const oldOnOpen = ws.onopen;
-        const oldOnMessage = ws.onmessage;
         const oldOnError = ws.onerror;
         const oldOnClose = ws.onclose;
 
